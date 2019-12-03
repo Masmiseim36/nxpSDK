d3d5a4739b837715167c6af2760ee6fc03875d9c
Markus Klein <masmiseim@gmx.de>
2019-12-03 12:01:29 +0100

    Removed files which were added by mistake Update iomux-Files

--- boards/evkmimxrt1010/driver_examples/lpspi/edma_b2b_transfer/master/Crossworks/lpspi_edma_b2b_transfer_master THUMB Debug/startup_MIMXRT1011.d
+++
@@ -1,2 +0,0 @@
-lpspi_edma_b2b_transfer_master\ THUMB\ Debug/startup_MIMXRT1011.o: \
- C:/Users/Markus/AppData/Local/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v4/packages/targets/iMXRT/startup_MIMXRT1011.s
--- boards/evkmimxrt1010/driver_examples/lpspi/edma_b2b_transfer/master/Crossworks/lpspi_edma_b2b_transfer_master THUMB Debug/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-lpspi_edma_b2b_transfer_master\ THUMB\ Debug/thumb_crt0.o: \
- C:/Program\ Files/Rowley\ CrossWorks\ for\ ARM\ 4.5/source/thumb_crt0.s
